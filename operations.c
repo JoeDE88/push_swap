f3b222431e7c83904b4bb0d9b008e11a41fce041
gblas-he <gblas-he@student.42madrid.com>
2026-03-02 20:09:47 +0100

    operationsv1

--- operations.c
+++ operations.c
@@ -6,12 +6,44 @@
 /*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/28 19:58:57 by gblas-he          #+#    #+#             */
-/*   Updated: 2026/02/28 21:42:06 by gblas-he         ###   ########.fr       */
+/*   Updated: 2026/03/02 20:09:10 by gblas-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sa(t_node **a)
+void	swap(t_node **lst, char *op)
 {
+	t_node *tmp;
+
+	if (!lst || !(*lst)->next)
+		return ;
+	tmp = *lst;
+	*lst = (*lst)->next;
+	tmp->next = (*lst)->next;
+	(*lst)->next = tmp;
+	if (op)
+		write(1, op, 3);
+}
+
+void	push(t_node **dest, t_node **src, char *op)
+{
+	t_node	*tmp;
+
+	if (!*src)
+		return ;
+
+	tmp = *src;
+	*src = (*src)->next;
+	tmp->next = *dest;
+	*dest = tmp;
+	if (op)
+		write(1, op, 3);
+}
+
+void ss(t_node **lst_a, t_node **lst_b)
+{
+	swap(lst_a, NULL);
+	swap(lst_b, NULL);
+	write(1, "ss\n", 3);
 }
---
+++ operations_rotate.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations_rotate.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/02 19:25:23 by gblas-he          #+#    #+#             */
+/*   Updated: 2026/03/02 19:56:01 by gblas-he         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	rotate(t_node **lst, char *op)
+{
+	t_node	*first;
+	t_node	*last;
+
+	if (!*lst || !(*lst)->next)
+		return ;
+	first = *lst;
+	last = *lst;
+	while (last->next)
+		last = last->next;
+	*lst = first->next;
+	first->next = NULL;
+	last->next = first;
+	if (op)
+		write(1, op, 3);
+}
+
+void	reverse_rotate(t_node **lst, char *op)
+{
+	t_node	*last;
+	t_node	*second_last;
+
+	if (!lst || !*lst || !(*lst)->next)
+		return ;
+	second_last = NULL;
+	last = *lst;
+	while (last->next)
+	{
+		second_last = last;      
+		last = last->next;       
+	}
+	second_last->next = NULL;
+	last->next = *lst;
+	*lst = last;
+	if (op)
+		write(1, op, 4);
+}
+
+void rr(t_node **lst_a, t_node **lst_b)
+{
+	rotate(lst_a, NULL);
+	rotate(lst_b, NULL);
+	write(1, "rr\n", 3);
+}
+
+void	rrr(t_node **lst_a, t_node **lst_b)
+{
+	reverse_rotate(lst_a, NULL);
+	reverse_rotate(lst_b, NULL);
+	write(1, "rrr\n", 4);
+}
