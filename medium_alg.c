5348a084a88d73dfcdad1ebadc6a39131be45cfa
Giovanni Blas Heredia <gblas-he@c2r12s1.42madrid.com>
2026-03-13 20:33:20 +0100

    chunksv3

--- medium_alg.c
+++ medium_alg.c
@@ -6,7 +6,7 @@
 /*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 17:19:35 by gblas-he          #+#    #+#             */
-/*   Updated: 2026/03/13 20:28:59 by gblas-he         ###   ########.fr       */
+/*   Updated: 2026/03/13 20:32:45 by gblas-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,31 +27,21 @@ int	main(void)
 	t_node *a;
 	__attribute__((unused)) t_node *b;
 	t_node *tmp;
-	t_node *node1;
-	t_node *node2;
-	t_node *node3;
-	t_node *node4;
-	t_node *node5;
-	t_node *node6;
 
 	a = NULL;
 	b = NULL;
-	node1 = lst_new(2);
-	node2 = lst_new(1);
-	node3 = lst_new(3);
-	node4 = lst_new(99);
-	node5 = lst_new(-1);
-	node6 = lst_new(0);
+
 	// Añadir nodos al frente
-	lst_addback(&a, node1);
-	lst_addback(&a, node2);
-	lst_addback(&a, node3);
-	lst_addback(&a, node4);
-	lst_addback(&a, node5);
-	lst_addback(&a, node6);
+	lst_addback(&a, lst_new(-1));
 	lst_addback(&a, lst_new(7));
+	lst_addback(&a, lst_new(5));
+	lst_addback(&a, lst_new(2));
+	lst_addback(&a, lst_new(3));
+	lst_addback(&a, lst_new(100));
+	lst_addback(&a, lst_new(34));
 	lst_addback(&a, lst_new(20));
 	lst_addback(&a, lst_new(-10));
+
 	// Imprimir la lista
 	tmp = a;
 	while (tmp)
