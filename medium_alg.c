10ce2911e8a6be946ed9777b2c7ea24ace06a2bf
Giovanni Blas Heredia <gblas-he@c2r10s2.42madrid.com>
2026-03-14 21:52:54 +0100

    chunksv3

--- medium_alg.c
+++ medium_alg.c
@@ -6,7 +6,7 @@
 /*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 17:19:35 by gblas-he          #+#    #+#             */
-/*   Updated: 2026/03/14 19:48:08 by gblas-he         ###   ########.fr       */
+/*   Updated: 2026/03/14 21:52:27 by gblas-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@ void	chunks_sort(t_node **a, __attribute__((unused)) t_node **b)
 {
 	int	size;
 	int	num_chunks;
-	int	resto;
 	int	chunks_size;
 
 	size = lst_size(*a);
+	/* 	if (size <= 100)
+			num_chunks = 5;
+		else
+			num_chunks = 11; */
 	num_chunks = ft_sqrt(size);
 	chunks_size = size / num_chunks;
 	if (size % num_chunks != 0)
-		num_chunks++;
-	printf("dentro de función: %d %d %d %d\n", size, num_chunks, resto,
-		chunks_size);
+		chunks_size++;
+	printf("dentro de función: %d %d %d\n", size, num_chunks, chunks_size);
 }
 
 int	main(void)
@@ -43,16 +45,6 @@ int	main(void)
 	lst_addback(&a, lst_new(5));
 	lst_addback(&a, lst_new(2));
 	lst_addback(&a, lst_new(3));
-	lst_addback(&a, lst_new(100));
-	lst_addback(&a, lst_new(-166));
-	lst_addback(&a, lst_new(140));
-	lst_addback(&a, lst_new(990));
-	lst_addback(&a, lst_new(34));
-	lst_addback(&a, lst_new(20));
-	lst_addback(&a, lst_new(-10));
-	lst_addback(&a, lst_new(-10));
-	lst_addback(&a, lst_new(-10));
-	lst_addback(&a, lst_new(-10));
 
 	// Imprimir la lista
 	tmp = a;
