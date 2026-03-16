7ce7a08b0c84f1989374c77f6f81e206626155fa
Giovanni Blas Heredia <gblas-he@c2r16s2.42madrid.com>
2026-03-16 16:35:55 +0100

    chunksv6

--- medium_alg.c
+++ medium_alg.c
@@ -6,16 +6,17 @@
 /*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 17:19:35 by gblas-he          #+#    #+#             */
-/*   Updated: 2026/03/16 15:43:01 by gblas-he         ###   ########.fr       */
+/*   Updated: 2026/03/16 16:34:32 by gblas-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-int find_max_pos(t_node *b)
+
+int	find_max_pos(t_node *b)
 {
-	int i;
-	int max_idx;
-	int max_pos;
+	int	i;
+	int	max_idx;
+	int	max_pos;
 
 	if (!b)
 		return (-1);
@@ -37,8 +38,8 @@ int find_max_pos(t_node *b)
 
 void	push_chunks(t_node **a, t_node **b, int chunks_size)
 {
-	int i;
-	
+	int	i;
+
 	i = 0;
 	while (*a)
 	{
@@ -55,27 +56,27 @@ void	push_chunks(t_node **a, t_node **b, int chunks_size)
 		}
 		else
 			ra(a);
-	}	
+	}
 }
 
-void push_back(t_node **a, t_node **b)
+void	push_back(t_node **a, t_node **b)
 {
-	int size;
-	int max_pos;
-	
+	int	size;
+	int	max_pos;
+
 	while (*b)
 	{
 		size = lst_size(*b);
 		max_pos = find_max_pos(*b);
 		if (max_pos <= size / 2)
 		{
-			while(max_pos--)
+			while (max_pos--)
 				rb(b);
 		}
 		else
 		{
 			max_pos = size - max_pos;
-			while(max_pos--)
+			while (max_pos--)
 				rrb(b);
 		}
 		pa(a, b);
@@ -84,95 +85,18 @@ void push_back(t_node **a, t_node **b)
 
 void	chunks_sort(t_node **a, t_node **b)
 {
-	int	size;
-	int	num_chunks;
-	int	chunks_size;
+	int size;
+	int num_chunks;
+	int chunks_size;
 
 	size = lst_size(*a);
 	if (size <= 100)
 		num_chunks = 5;
-    else
+	else
 		num_chunks = 11;
 	chunks_size = size / num_chunks;
 	if (size % num_chunks)
 		chunks_size++;
 	push_chunks(a, b, chunks_size);
 	push_back(a, b);
-}
-
-
-
-int	main(void)
-{
-	t_node *a;
-	t_node *b;
-	t_node *tmp;
-
-	a = NULL;
-	b = NULL;
-
-	// Añadir nodos al frente
-	lst_addback(&a, lst_new(0));
-	lst_addback(&a, lst_new(4));
-	lst_addback(&a, lst_new(99));
-	lst_addback(&a, lst_new(-1));
-	lst_addback(&a, lst_new(2));
-	lst_addback(&a, lst_new(200));
-
-	// Imprimir la lista
-	printf("Lista original:\n");
-	tmp = a;
-	while (tmp)
-	{
-		printf("%d", tmp->value);
-		if (tmp->next)
-			printf(" -> ");
-		tmp = tmp->next;
-	}
-
-	printf("\n\nLista con indices:\n");
-
-	index_list(&a);
-
-	tmp = a;
-	while (tmp)
-	{
-		printf("%d, (%d)", tmp->value, tmp->idx);
-		if (tmp->next)
-			printf(" -> ");
-		tmp = tmp->next;
-	}
-
-	chunks_sort(&a, &b);
-
-	printf("\nResultado final:\n");
-
-	tmp = a;
-	while (tmp)
-	{
-		printf("%d (%d)", tmp->value, tmp->idx);
-		if (tmp->next)
-			printf(" -> ");
-		tmp = tmp->next;
-	}
-
-	//printf("\n");
-
-	// printf("Maxima posición:%d\n", find_max_pos(a));
-
-	// Liberar memoria
-	while (a)
-	{
-		tmp = a->next;
-		free(a);
-		a = tmp;
-	}
-		while (b)
-	{
-		tmp = b->next;
-		free(b);
-		b = tmp;
-	}
-
-	return (0);
 }
