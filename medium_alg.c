5c5a132bd8086fa88cfe48b5c1f241f25e2e3521
gblas-he <gblas-he@student.42madrid.com>
2026-03-16 15:41:29 +0100

    chunksv5

--- medium_alg.c
+++ medium_alg.c
@@ -6,29 +6,39 @@
 /*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 17:19:35 by gblas-he          #+#    #+#             */
-/*   Updated: 2026/03/15 19:24:00 by gblas-he         ###   ########.fr       */
+/*   Updated: 2026/03/16 15:39:50 by gblas-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	chunks_sort(t_node **a, __attribute__((unused)) t_node **b)
+int find_max_pos(t_node *b)
 {
-	int	size;
-	int	num_chunks;
-	int	chunks_size;
-	int	i;
+	int i;
+	int max_idx;
+	int max_pos;
 
-	size = lst_size(*a);
-	/* 	if (size <= 100)
-			num_chunks = 5;
-		else
-			num_chunks = 11;
-		chunks_size = size / num_chunks; */
-	num_chunks = ft_sqrt(size);
-	chunks_size = size / num_chunks;
-	if (size % num_chunks != 0)
-		chunks_size++;
+	if (!b)
+		return (-1);
+	i = 0;
+	max_idx = b->idx;
+	max_pos = 0;
+	while (b)
+	{
+		if (b->idx > max_idx)
+		{
+			max_idx = b->idx;
+			max_pos = i;
+		}
+		b = b->next;
+		i++;
+	}
+	return (max_pos);
+}
+
+void	push_chunks(t_node **a, t_node **b, int chunks_size)
+{
+	int i;
+	
 	i = 0;
 	while (*a)
 	{
@@ -44,17 +54,58 @@ void	chunks_sort(t_node **a, __attribute__((unused)) t_node **b)
 			i++;
 		}
 		else
-		{
 			ra(a);
-		}
-	}
-	printf("\ndentro de función: %d %d %d\n", size, num_chunks, chunks_size);
+	}	
 }
+
+void push_back(t_node **a, t_node **b)
+{
+	int size;
+	int max_pos;
+	
+	while (*b)
+	{
+		size = lst_size(*b);
+		max_pos = find_max_pos(*b);
+		if (max_pos <= size / 2)
+		{
+			while(max_pos--)
+				rb(b);
+		}
+		else
+		{
+			max_pos = size - max_pos;
+			while(max_pos--)
+				rrb(b);
+		}
+		pa(a, b);
+	}
+}
+
+void	chunks_sort(t_node **a, t_node **b)
+{
+	int	size;
+	int	num_chunks;
+	int	chunks_size;
+
+	size = lst_size(*a);
+	if (size <= 100)
+		num_chunks = 5;
+    else
+		num_chunks = 11;
+	chunks_size = size / num_chunks;
+	if (size % num_chunks)
+		chunks_size++;
+	push_chunks(a, b, chunks_size);
+	push_back(a, b);
+}
+
+
 
 int	main(void)
 {
 	t_node *a;
-	__attribute__((unused)) t_node *b;
+	t_node *b;
 	t_node *tmp;
 
 	a = NULL;
@@ -69,6 +120,7 @@ int	main(void)
 	lst_addback(&a, lst_new(200));
 
 	// Imprimir la lista
+	printf("Lista original:\n");
 	tmp = a;
 	while (tmp)
 	{
@@ -78,11 +130,10 @@ int	main(void)
 		tmp = tmp->next;
 	}
 
-	printf("\n");
+	printf("\n\nLista con indices:\n");
 
 	index_list(&a);
 
-	// Imprimir la lista
 	tmp = a;
 	while (tmp)
 	{
@@ -94,6 +145,19 @@ int	main(void)
 
 	chunks_sort(&a, &b);
 
+	printf("\nResultado final:\n");
+
+	tmp = a;
+	while (tmp)
+	{
+		printf("%d (%d)", tmp->value, tmp->idx);
+		if (tmp->next)
+			printf(" -> ");
+		tmp = tmp->next;
+	}
+
+	printf("\n");
+
 	// printf("Maxima posición:%d\n", find_max_pos(a));
 
 	// Liberar memoria
@@ -103,4 +167,12 @@ int	main(void)
 		free(a);
 		a = tmp;
 	}
+		while (b)
+	{
+		tmp = b->next;
+		free(b);
+		b = tmp;
+	}
+
+	return (0);
 }
