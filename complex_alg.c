ce90e1d9b94965bd63b3bb587e88fa834abef8cf
Giovanni Blas Heredia <gblas-he@c2r16s6.42madrid.com>
2026-03-18 20:32:59 +0100

    radixv2

--- complex_alg.c
+++ complex_alg.c
@@ -6,60 +6,61 @@
 /*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/07 15:11:02 by gblas-he          #+#    #+#             */
-/*   Updated: 2026/03/17 20:06:12 by gblas-he         ###   ########.fr       */
+/*   Updated: 2026/03/18 20:32:15 by gblas-he         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/* static int	get_max_bits(t_node **stack)
+int	get_max_bits(t_node *a)
 {
-	t_node	*head;
-	int		max;
-	int		max_bits;
+	int	max_idx;
 
-	head = *stack;
-	max = head->idx;
+	__attribute__((unused)) int max_bits;
 	max_bits = 0;
-	while (head)
+	max_idx = a->idx;
+	while (a)
 	{
-		if (head->idx > max)
-			max = head->idx;
-		head = head->next;
+		if (a->idx > max_idx)
+			max_idx = a->idx;
+		a = a->next;
 	}
-	while ((max >> max_bits) != 0)
+	while (max_idx >> max_bits != 0)
 		max_bits++;
 	return (max_bits);
 }
 
-void	radix_sort(t_node **stack_a, t_node **stack_b)
+void	radix_sort(t_node **a, t_node **b)
 {
-	t_node	*head_a;
-	int		i;
-	int		j;
 	int		size;
 	int		max_bits;
+	t_node	*tmp;
+	int		i;
+	int		j;
 
+	size = lst_size(*a);
+	max_bits = get_max_bits(*a);
+	tmp = *a;
 	i = 0;
-	head_a = *stack_a;
-	size = lst_size(head_a);
-	max_bits = get_max_bits(stack_a);
+	printf("\npruebas: %d\n", max_bits);
 	while (i < max_bits)
 	{
 		j = 0;
 		while (j++ < size)
 		{
-			head_a = *stack_a;
-			if (((head_a->idx >> i) & 1) == 1)
-				ra(stack_a);
+			if (((tmp->idx >> i) & 1) == 1)
+				ra(a);
 			else
-				pb(stack_a, stack_b);
+				pb(a, b);
 		}
-		while (lst_size(*stack_b) != 0)
-			pa(stack_a, stack_b);
+		while (*b)
+		{
+			pa(a, b);
+			*b = (*b)->next;
+		}
 		i++;
 	}
-} */
+}
 
 int	main(void)
 {
@@ -76,7 +77,10 @@ int	main(void)
 	lst_addback(&a, lst_new(99));
 	lst_addback(&a, lst_new(-1));
 	lst_addback(&a, lst_new(2));
-	lst_addback(&a, lst_new(200));
+	lst_addback(&a, lst_new(300));
+	lst_addback(&a, lst_new(400));
+	lst_addback(&a, lst_new(-200));
+	lst_addback(&a, lst_new(1000));
 
 	// Imprimir la lista
 	printf("Lista original:\n");
@@ -102,20 +106,20 @@ int	main(void)
 		tmp = tmp->next;
 	}
 
-	//printf("\nMaximo bits: %d\n", get_max_bits(&a));
+	// printf("\nMaximo bits: %d\n", get_max_bits(&a));
 
-	radix_sort(&a,&b);
+	radix_sort(&a, &b);
 
-	/* 	printf("\nResultado final:\n");
+	printf("\nResultado final:\n");
 
-		tmp = a;
-		while (tmp)
-		{
-			printf("%d (%d)", tmp->value, tmp->idx);
-			if (tmp->next)
-				printf(" -> ");
-			tmp = tmp->next;
-		} */
+	tmp = a;
+	while (tmp)
+	{
+		printf("%d (%d)", tmp->value, tmp->idx);
+		if (tmp->next)
+			printf(" -> ");
+		tmp = tmp->next;
+	}
 
 	// Liberar memoria
 	while (a)
